280a7fa267964ce6fe7c1174e803bd067fda091f
Jintzo <me@jintzo.me>
2017-06-19 10:20:42 +0200

    language dropdown

---
+++ src/components/LanguageDropdown.vue
@@ -0,0 +1,50 @@
+<template>
+  <span class="" :class="{ open: show, dropdown: !dropup, dropup: dropup}">
+    <button class="button dropdown-button" @click="toggle($event)" :disabled="disabled">
+      <i class="fa fa-chevron-down dropdown-caret"></i>
+      {{ $t('general.language') }}
+    </button>
+    <ul class="dropdown-menu">
+      <li @click="clicked('en')" class="dropdown-item">English</li>
+      <li @click="clicked('de')" class="dropdown-item">Deutsch</li>
+    </ul>
+  </span>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      show: false,
+      dropup: false
+    }
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    switchLanguage (lang) {
+      let current = this.$locale.current()
+
+      if (current !== lang) {
+        this.$locale.change(lang)
+      }
+    },
+    toggle (e) {
+      this.show = !this.show
+    },
+    clicked (lang) {
+      this.switchLanguage(lang)
+      this.show = false
+    }
+  },
+  events: {
+    'hide::dropdown' () {
+      this.show = false
+    }
+  }
+}
+</script>
