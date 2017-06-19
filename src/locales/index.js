fc67785bab560d7ca3f326f2837e9154b9c3fe5a
Jintzo <me@jintzo.me>
2017-06-19 10:18:52 +0200

    locales

---
+++ src/locales/index.js
@@ -0,0 +1,38 @@
+export default {
+  en: {
+    general: {
+      title: 'Grainform Analysis Platform',
+      database: 'Database',
+      select: 'Select...',
+      loading: 'Loading...',
+      hello: 'Hello {name}!',
+      name: 'Name',
+      refresh: 'Refresh',
+      success: 'Success',
+      error: 'Error',
+      confirm: 'Confirm',
+      remove: 'Remove',
+      language: 'Language',
+      mail: 'Mail',
+      password: 'Password',
+      user: 'User',
+      logout: 'Logout'
+    },
+    dummy: {
+      user: 'John Doe',
+      password: 'doejohn123'
+    },
+    errors: {
+      apiError: 'Could not reach API'
+    },
+    auth: {
+      login: 'Log in',
+      loginMessage: 'Please log in to be able to use the grainform analysis platform.',
+      register: 'Register',
+      registerMessage: 'Register here to be able to use the grainform analysis platform. Note that an administrator will have to unlock your account after registration.',
+      passwordConfirm: 'Confirm password',
+      registerAs: 'Register as',
+      registerSuccess: 'Successfully registered - please wait for an administrator to confirm.'
+    }
+  }
+}
