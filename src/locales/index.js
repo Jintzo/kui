10b5b18ac3684e6a5988d173628445d0430ff87d
Jintzo <me@jintzo.me>
2017-06-21 19:10:03 +0200

    new texts

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,7 +3,7 @@ export default {
     general: {
       title: 'Grainform Analysis Platform',
       database: 'Database',
-      select: 'Select...',
+      selectDatabase: 'Select Database...',
       loading: 'Loading...',
       hello: 'Hello {name}!',
       name: 'Name',
@@ -28,11 +28,12 @@ export default {
     auth: {
       login: 'Log in',
       loginMessage: 'Please log in to be able to use the grainform analysis platform.',
+      loginSuccess: 'Successfully logged in!',
       register: 'Register',
       registerMessage: 'Register here to be able to use the grainform analysis platform. Note that an administrator will have to unlock your account after registration.',
       passwordConfirm: 'Confirm password',
       registerAs: 'Register as',
-      registerSuccess: 'Successfully registered - please wait for an administrator to confirm.'
+      registerSuccess: 'Successfully registered -- please wait for an administrator to confirm.'
     }
   }
 }
