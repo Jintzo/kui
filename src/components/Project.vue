7d5f12d70dbbc20c27b3e19cc28a8d1f8e406d1c
Jintzo <me@jintzo.me>
2017-06-26 18:53:21 +0200

    added skeleton project template

---
+++ src/components/Project.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="entrypoint-project">
+    <ErrorBox v-bind:error="error"></ErrorBox>
+    <h2>{{ project.name }} </h2>
+    <LoadingBox v-if="isLoading"></LoadingBox>
+  </div>
+</template>
+
+<script>
+// import project middleware
+import project from '../middleware/project'
+
+// import child components
+import ErrorBox from './ErrorBox.vue'
+import LoadingBox from './LoadingBox.vue'
+
+export default {
+
+  /**
+   * vue data object method
+   * @return {Object} data object
+   */
+  data () {
+    return {
+      isLoading: false,
+      errorHidden: true,
+      error: '',
+      project: {}
+    }
+  },
+
+  props: [ 'id' ],
+
+  beforeMount () {
+    project.getProject(this, this.id, localStorage.getItem('database'), localStorage.getItem('token'))
+  },
+
+  components: {
+    ErrorBox, LoadingBox
+  }
+}
+</script>
